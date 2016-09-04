7fe19f02cce97aea53759068971a63f82e0addc0
VictorXiao <843731025@qq.com>
2016-09-05 07:41:54 +0800

    Initial commit

---
+++ app/src/main/java/com/victor_xiao/activitylifecycletest/DialogActivity.java
@@ -0,0 +1,18 @@
+package com.victor_xiao.activitylifecycletest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by Victor_Xiao on 16/07/24.
+ */
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_layout);
+    }
+}
